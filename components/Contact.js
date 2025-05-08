9896a7f47fda56e306069a75c78c59d75787b744
komorebi <adrianraycasas@gmail.com>
2025-05-08 22:32:44 +0400

    added CV and other details in experience section

---
+++ components/Contact.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div className="flex flex-row justify-center">
+
+        <div className="wrapper flex flex-row py-40">
+
+            <div className="basis-1/2 text-left">
+                <h1 className="text-komorebi-light text-9xl font-bold">Get in touch with me </h1>
+                <p className="text-gray-300 text-2xl leading-loose py-5 mb-10"> Got questions, suggestions, or just want to say hello? I'm all ears! Drop me a message or reach out via email. I'd love to assist you with anything you need.</p>
+
+                <h2 className="text-komorebi-lgreen text-6xl font-bold hover:underline duration-500 mt-5"> adrianraycasas@gmail.com </h2> 
+            </div>
+        </div>
+        
+
+    </div>
+  )
+}
+
+export default Contact
---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="bg-komorebi-dark-3 py-10 flex flex-row justify-around align-around rounded-footer">
+        <div className="text-gray-500 font-bold">© 2024 ADRIAN RAY CASAS</div>
+      
+    </div>
+  )
+}
+
+export default Footer
